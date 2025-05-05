afbd5a36190a6ac4303740a9faac5d927d8343ed
PickleLily <lily.bromberger@gmail.com>
2025-05-05 15:35:28 -0400

    initialization

--- FinalProj/app/src/main/java/com/bignerdranch/android/final_proj/MainActivity.kt
+++ FinalProj/app/src/main/java/com/bignerdranch/android/final_proj/MainActivity.kt
@@ -1,7 +1,12 @@
-git package com.bignerdranch.android.final_proj
+package com.bignerdranch.android.final_proj
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.util.Log
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,9 +18,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bignerdranch.android.final_proj.ui.theme.FinalProjTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompactActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val startButton = findViewById<>(R.id.start_button)
+
+        startButton.setOnClickListener{
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
         enableEdgeToEdge()
         setContent {
             FinalProjTheme {
